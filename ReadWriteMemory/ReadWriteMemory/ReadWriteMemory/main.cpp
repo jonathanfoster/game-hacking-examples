246d26d9091a07c328ab560d6f125d0cf4e65af8
Jonathan Foster <jonathan.e.foster@gmail.com>
2016-12-28 21:12:39 -0500

    Get process ID from file name

--- ReadWriteMemory/ReadWriteMemory/ReadWriteMemory/main.cpp
+++ ReadWriteMemory/ReadWriteMemory/ReadWriteMemory/main.cpp
@@ -2,11 +2,40 @@
 #include <Windows.h>
 #include <tlhelp32.h>
 
+DWORD GetProcessIdFromFileName(std::wstring fileName)
+{
+    DWORD pid;
+    PROCESSENTRY32 entry;
+    entry.dwSize = sizeof(entry);
+
+    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+
+    if (Process32First(snapshot, &entry) == TRUE)
+    {
+        while (Process32Next(snapshot, &entry) == TRUE)
+        {
+            std::wstring binPath = entry.szExeFile;
+
+            if (binPath.find(fileName) != std::wstring::npos) 
+            {
+                pid = entry.th32ProcessID;
+                break;
+            }
+        }
+    }
+
+    CloseHandle(snapshot);
+
+    return pid;
+}
+
 DWORD GetProcessIdFromWindowName(LPCWSTR windowName)
 {
     DWORD pid;
+
     HWND window = FindWindow(NULL, windowName);
     GetWindowThreadProcessId(window, &pid);
+    
     return pid;
 }
 
@@ -20,12 +49,15 @@ int main()
         FALSE, 
         GetCurrentProcessId());
 
-    // Get current window name
+    // Get window name
     wchar_t windowName[1024];
     GetConsoleTitle(&windowName[0], 1024);
 
     DWORD pid = GetProcessIdFromWindowName(windowName);
-    printf("Current PID is %d\n", pid);
+    printf("PID from window name is %d\n", pid);
+
+    pid = GetProcessIdFromFileName(L"ReadWriteMemory.exe");
+    printf("PID from file name is %d\n", pid);
 
     return 0;
 }
